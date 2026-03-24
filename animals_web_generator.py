a37b1445348117dfaea4a3aef4b1afb63a785646
YPCramer <yorkphilipp.cramer@gmail.com>
2026-03-24 11:03:43 +0100

    added one more field to animal cards

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -4,33 +4,40 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
+
 animals_data = load_data('animals_data.json')
 
-# Step 2
-output = ""
-for animal in animals_data:
+
+def serialize_animal(animal_obj):
+    output = ""
     output += '<li class="cards__item">\n'
-    if "name" in animal:
-        output += f'<div class="card__title">{animal["name"]}</div>\n'
+    if "name" in animal_obj:
+        output += f'<div class="card__title">{animal_obj["name"]}</div>\n'
     output += '  <p class="card__text">\n'
-    if "diet" in animal["characteristics"]:
-        output += f'<strong>Diet:</strong>{animal["characteristics"]["diet"]}<br/>\n'
-    if "locations" in animal:
-        output += f'<strong>Location:</strong>{animal["locations"][0]}<br/>\n'
-    if "type" in animal["characteristics"]:
-        output += f'<strong>Type:</strong>{animal["characteristics"]["type"]}<br/>\n'
+    if "diet" in animal_obj["characteristics"]:
+        output += f'<strong>Diet:</strong>{animal_obj["characteristics"]["diet"]}<br/>\n'
+    if "locations" in animal_obj:
+        output += f'<strong>Location:</strong>{animal_obj["locations"][0]}<br/>\n'
+    if "type" in animal_obj["characteristics"]:
+        output += f'<strong>Type:</strong>{animal_obj["characteristics"]["type"]}<br/>\n'
+    if "group" in animal_obj["characteristics"]:
+        output += f'<strong>Group:</strong>{animal_obj["characteristics"]["group"]}<br/>\n'
     output += '  </p>\n'
     output += "</li>\n"
+    return output
+
+output = ""
+for animal_obj in animals_data:
+    output += serialize_animal(animal_obj)
 
 
-# Step 1
 with open ("animals_template.html", "r") as fileobj:
     template = fileobj.read()
 
-# Step 3
+
 new_html = template.replace("__REPLACE_ANIMALS_INFO__", output)
 
-# Step 4
+
 with open("animals.html", "w") as write_object:
     write_object.write(new_html)
 
