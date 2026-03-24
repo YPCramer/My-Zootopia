1fd7a619ee7237ac8432d383e645844cafe42eb3
YPCramer <yorkphilipp.cramer@gmail.com>
2026-03-24 11:31:58 +0100

    Python file changed to make sure user can filter for skin type

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,11 +1,17 @@
 import json
 
+
 def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
 
-animals_data = load_data('animals_data.json')
+def get_skin_type(animals_data):
+    skin_types = set()
+    for animal in animals_data:
+        if "skin_type" in animal["characteristics"]:
+            skin_types.add(animal["characteristics"]["skin_type"])
+    return skin_types
 
 
 def serialize_animal(animal_obj):
@@ -14,30 +20,51 @@ def serialize_animal(animal_obj):
     if "name" in animal_obj:
         output += f'<div class="card__title">{animal_obj["name"]}</div>\n'
     output += '  <p class="card__text">\n'
+    output += '   <ul>\n'
     if "diet" in animal_obj["characteristics"]:
-        output += f'<strong>Diet:</strong>{animal_obj["characteristics"]["diet"]}<br/>\n'
+        output += f'        <li><strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}</li>\n'
     if "locations" in animal_obj:
-        output += f'<strong>Location:</strong>{animal_obj["locations"][0]}<br/>\n'
+        output += f'        <li><strong>Location:</strong> {animal_obj["locations"][0]}</li>\n'
     if "type" in animal_obj["characteristics"]:
-        output += f'<strong>Type:</strong>{animal_obj["characteristics"]["type"]}<br/>\n'
+        output += f'        <li><strong>Type:</strong> {animal_obj["characteristics"]["type"]}</li>\n'
     if "group" in animal_obj["characteristics"]:
-        output += f'<strong>Group:</strong>{animal_obj["characteristics"]["group"]}<br/>\n'
+        output += f'        <li><strong>Group:</strong> {animal_obj["characteristics"]["group"]}</li>\n'
+    output += '    </ul>\n'
     output += '  </p>\n'
-    output += "</li>\n"
+    output += '</li>\n'
     return output
 
-output = ""
-for animal_obj in animals_data:
-    output += serialize_animal(animal_obj)
+
+def main():
+    animals_data = load_data('animals_data.json')
+    skin_types = get_skin_type(animals_data)
+    print("Available skin types: ")
+    for skin_type in skin_types:
+        print(f"- {skin_type}")
+    user_skin_type = input("\nEnter a skin type: ")
+    output = ""
+    for animal in animals_data:
+        if "skin_type" in animal["characteristics"]:
+            if animal["characteristics"]["skin_type"].lower() == user_skin_type.lower():
+                output += serialize_animal(animal)
 
 
-with open ("animals_template.html", "r") as fileobj:
-    template = fileobj.read()
+    with open("animals_template.html", "r") as fileobj:
+        template = fileobj.read()
+
+    new_html = template.replace("__REPLACE_ANIMALS_INFO__", output)
+
+    with open("animals.html", "w") as write_object:
+        write_object.write(new_html)
+
+    print(f"Website generated for skin_type: {user_skin_type}")
+
+if __name__ == "__main__":
+    main()
 
 
-new_html = template.replace("__REPLACE_ANIMALS_INFO__", output)
 
 
-with open("animals.html", "w") as write_object:
-    write_object.write(new_html)
+
+
 
