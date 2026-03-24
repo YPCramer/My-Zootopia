4c68c8d6e30a717a2c82ce7ed5d2192f7ddc1500
YPCramer <yorkphilipp.cramer@gmail.com>
2026-03-24 09:08:03 +0100

    Made changes to script that reads content of json file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,11 +1,9 @@
 import json
 
 def load_data(file_path):
-    """Loads a JSON file"""
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-# Read the file
 animals_data = load_data('animals_data.json')
 
 for animal in animals_data:
